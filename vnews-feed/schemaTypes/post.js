677037c3f91dfce6a1fe4a90b62ac86b4402ea24
Mr Punk da Silva <gustavojs417@gmail.com>
2024-12-28 17:17:49 -0300

    setup sanity

---
+++ vnews-feed/schemaTypes/post.js
@@ -0,0 +1,14 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+  ],
+})
