bd1775f515c645c7c74b1698981ae0cca825f850
Mr Punk da Silva <gustavojs417@gmail.com>
2025-03-20 09:01:58 -0300

    added new schemas

--- vnews-feed/schemaTypes/post.js
+++ vnews-feed/schemaTypes/post.js
@@ -1,14 +1,38 @@
-import {defineField, defineType} from 'sanity'
-
-export default defineType({
-  name: 'post',
-  title: 'Post',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-  ],
-})
+export default {
+	title: 'Post',
+	name: 'post',
+	type: 'document',
+	fields: [
+		{
+			title: 'Title',
+			name: 'title',
+			type: 'string',
+			validation: Rule => [
+				Rule.required().min(10).error("Title must be at least 10 characters long"),
+				Rule.required().max(50).warning("It's better to use short snappy titles")
+			]
+		},
+		{
+			title: 'Author',
+			name: 'author',
+			type: 'reference',
+			to: [{type: 'author'}]
+		},
+		{
+			title: 'Excerpt',
+			name: 'excerpt',
+			type: 'text',
+			validation: Rule => Rule.max(120).error("Excerpt must be less than 120 characters long")
+		},
+		{
+			title: 'Content',
+			name: 'content',
+			type: 'text'
+		},
+		{
+			title: 'Image',
+			name: 'image',
+			type: 'image'
+		}
+	]
+}
