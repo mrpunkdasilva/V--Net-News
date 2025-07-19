4c95f4a8a139314bfe59baae71b07faf1ce8078a
Mr Punk da Silva <gustavojs417@gmail.com>
2025-07-19 14:39:24 -0300

    theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     content: [],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'win-gray': '#c0c0c0',
+                'win-dark-gray': '#808080',
+                'win-light-gray': '#dfdfdf',
+                'win-black': '#000000',
+                'win-white': '#ffffff',
+                'win-blue': '#008080',
+            },
+        },
     },
     plugins: [],
 }
