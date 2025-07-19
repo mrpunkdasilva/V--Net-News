d7b0b891f18572a99dae79a5116a8963e4574094
Mr Punk da Silva <gustavojs417@gmail.com>
2025-07-19 14:41:01 -0300

    menuitem

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -5,14 +5,32 @@ export default {
 </script>
 
 <template>
-  <router-link :to="to" class="flex items-center p-4 border-r-4 border-transparent text-gray-100 hover:text-white hover:bg-gray-900">
+  <router-link
+    :to="to"
+    class="menu-item"
+  >
     <slot />
   </router-link>
 </template>
 
 <style scoped>
-.router-link-exact-active {
-  @apply border-green-500 bg-gray-900;
+.menu-item {
+  @apply flex items-center p-2 border-2 border-transparent bg-win-gray text-win-black;
+  border-top-color: #ffffff;
+  border-left-color: #ffffff;
+  border-bottom-color: #000000;
+  border-right-color: #000000;
+}
 
+.menu-item:hover {
+  @apply bg-win-dark-gray text-win-white;
+}
+
+.router-link-exact-active {
+  @apply bg-win-gray;
+  border-top-color: #000000;
+  border-left-color: #000000;
+  border-bottom-color: #ffffff;
+  border-right-color: #ffffff;
 }
 </style>
